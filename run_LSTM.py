7ff83c61761548a0f1af19a9021368cd3603a088
Túlio Corrêa Loures <tulicloure12@yahoo.com.br>
2020-05-01 15:10:45 -0300

    fixing identation

--- run_LSTM.py
+++ run_LSTM.py
@@ -232,7 +232,7 @@ if __name__ == '__main__':
 	print("Training tokenizer...")
 	tokenizer, word_index = train_tokenizer(train_data.concatenated_m)
 	
-	 hyperparemeter_search(
+	hyperparemeter_search(
 		  EMBEDDING_DIM, MAX_WORDS, learning_rates, optimizers, tokenizer, train_data,
 		  val_data, test_data, word_index, outpath
 	 )

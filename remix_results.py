35eac43af6bd02a0dc569b9ad79a8e2ea8b6716b
Túlio Corrêa Loures <tulicloure12@yahoo.com.br>
2020-05-03 16:20:52 -0300

    flatten score lists

--- remix_results.py
+++ remix_results.py
@@ -17,11 +17,12 @@ if __name__ == '__main__':
 		df_gt = pd.read_csv('../ChAT/train_truth.csv')
 		for fname in fnames:
 			df_preds = pd.read_csv(fname, index_col=0)
-			df_gt = pd.merge( df_gt, df_preds, on=['channel','user'], suffixes=['',fname.stem+'::'] )
+			df_gt = pd.merge( df_gt, df_preds, on=['channel','user'], suffixes=['','::'+fname.stem] )
 		df_gt.to_csv( merged_f, index=False )
 	
 	scores = {}
 	for col in df_gt.columns[3:]:	
-			scores[col] = sklearn.metrics.precision_recall_fscore_support( y_true=df_gt['subscribed'], y_pred=df_gt[col] )
-	df_scores = pd.DataFrame.from_dict( scores, columns=['precision','recall','fscore','support'], orient='index' )
+			sc = sklearn.metrics.precision_recall_fscore_support( y_true=df_gt['subscribed'], y_pred=df_gt[col] )
+			scores[col] = [ item for sublist in sc for item in sublist ]
+	df_scores = pd.DataFrame.from_dict( scores, columns=['0precision','1precision','0recall','1recall','0fscore','1fscore','0support','1support'], orient='index' )
 	df_scores.to_csv( 'result_scores.csv', index_label='run' )

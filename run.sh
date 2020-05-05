1be8989db26c5452209eea23e4f43cf35e16615f
Túlio Corrêa Loures <tulicloure12@yahoo.com.br>
2020-05-05 20:31:38 -0300

    script now splits input file

--- run.sh
+++ run.sh
@@ -3,13 +3,16 @@
 INFILE=$1 #"timestamps/train (2).csv"
 OUTPATH=$2 #"predictions/"
 
+mkdir "splitted"
+
+tail -n +2 $INFILE | split -l 10000 - --filter='sh -c "{ head -n1 $INFILE; cat; } > $FILE"' "splitted/x"
+
 mkdir "prepared"
 
-INNLINES=$(wc -l < "$INFILE")
+python prepare_data.py all "splitted" "prepared" False
 
-python prepare_data.py file $INFILE "prepared" False
+#INNLINES=$(wc -l < "$INFILE")
+
 #python run_LSTM.py hsearch "prepared" $OUTPATH $INNLINES
 #python batched_model.py train "prepared/train0001.csv" 100000 "prepared/train0000.csv" 100000 $OUTPATH
 python batched_model.py test "prepared" $OUTPATH
-
-#tail -n +2 train0000.csv | split -l 50 - --filter='sh -c "{ head -n1 train0000.csv; cat; } > $FILE"' x

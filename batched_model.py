77f067f54462584bfe2ce9d6f91d7fee853282c3
Túlio Corrêa Loures <tulicloure12@yahoo.com.br>
2020-05-05 19:22:29 -0300

    testing batch size 100000

--- batched_model.py
+++ batched_model.py
@@ -194,7 +194,7 @@ def train_batched( trainfile, trainlines, valfile, vallines, feature_list ):
 	opt = keras.optimizers.Adagrad
 	lr = 0.001
 	
-	batch_size = 10000
+	batch_size = 100000
 	
 	print("Training tokenizer...")
 	tokenizer, word_index = train_tokenizer( trainfile, batch_size, trainlines )

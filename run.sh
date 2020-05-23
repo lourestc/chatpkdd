cbdf7e8b7a93ecef4dfc4acba9116d92782cc627
Túlio Corrêa Loures <tulicloure12@yahoo.com.br>
2020-05-23 02:16:46 -0300

    added script call to activate conda env

--- run.sh
+++ run.sh
@@ -3,16 +3,19 @@
 INFILE=$1 #"timestamps/train (2).csv"
 OUTPATH=$2 #"predictions/"
 
+conda activate pkddchat
+
 mkdir "splitted"
 
-tail -n +2 $INFILE | split -l 10000 - --filter='sh -c "{ head -n1 $INFILE; cat; } > $FILE"' "splitted/x"
+FLINES=100000
+tail -n +2 $INFILE | split -l $FLINES - --filter='sh -c "{ head -n1 $INFILE; cat; } > $FILE"' "splitted/x"
 
-mkdir "prepared"
+mkdir "test_prepared"
 
-python prepare_data.py all "splitted" "prepared" False
+python prepare_data.py all "splitted" "test_prepared" False
 
 #INNLINES=$(wc -l < "$INFILE")
 
 #python run_LSTM.py hsearch "prepared" $OUTPATH $INNLINES
-#python batched_model.py train "prepared/train0001.csv" 100000 "prepared/train0000.csv" 100000
-python batched_model.py test "prepared" $OUTPATH
+#python batched_model.py train "prepared/train_split.csv" 20000000 "prepared/val_split.csv" 9539420
+python batched_model.py test "test_prepared" $OUTPATH

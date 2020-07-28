5774b4800af4832cee0e0475e60ff8ba8bb6dd32
Túlio Corrêa Loures <tulicloure12@yahoo.com.br>
2020-07-28 17:00:45 -0300

    added script to separate test into known and unknown users

---
+++ find_unknown_users.py
@@ -0,0 +1,14 @@
+from prepare_data import *
+
+if __name__ == '__main__':
+
+	trainfile = sys.argv[1]
+	intestfile = sys.argv[2]
+	outtestfile1 = sys.argv[3]
+	outtestfile2 = sys.argv[4]
+	
+	train_df = read_data(trainfile)
+	test_df = read_data(intestfile)
+	
+	test_df[test_df.user.isin(train_df.user)].to_csv(outtestfile1, index=False)
+	test_df[~test_df.user.isin(train_df.user)].to_csv(outtestfile2, index=False)

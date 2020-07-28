c1c5c263d55a73ab8bf21bdc4230747277c920c2
Túlio Corrêa Loures <tulicloure12@yahoo.com.br>
2020-07-28 17:31:30 -0300

    make it also work for channels

--- find_unknown_users.py
+++ find_unknown_users.py
@@ -1,15 +1,22 @@
 from batched_model import *
 
-#python3 find_unknown_users.py train_prepared/train0000.csv test_prepared/test.csv test_prepared/test_kn.csv test_prepared/test_unkn.csv
+#python3 find_unknown_users.py user train_prepared/train0000.csv test_prepared/test.csv test_prepared/test_u_kn.csv test_prepared/test_u_unkn.csv
+#python3 find_unknown_users.py channel train_prepared/train0000.csv test_prepared/test.csv test_prepared/test_c_kn.csv test_prepared/test_c_unkn.csv
 if __name__ == '__main__':
 
-	trainfile = sys.argv[1]
-	intestfile = sys.argv[2]
-	outtestfile1 = sys.argv[3]
-	outtestfile2 = sys.argv[4]
+	field = sys.argv[1]
+	trainfile = sys.argv[2]
+	intestfile = sys.argv[3]
+	outtestfile1 = sys.argv[4]
+	outtestfile2 = sys.argv[5]
 	
 	train_df = read_data(trainfile)
 	test_df = read_data(intestfile)
 	
-	test_df[test_df.user.isin(train_df.user)].to_csv(outtestfile1, index=False)
-	test_df[~test_df.user.isin(train_df.user)].to_csv(outtestfile2, index=False)
+	if field=='user':
+		test_df[test_df.user.isin(train_df.user)].to_csv(outtestfile1, index=False)
+		test_df[~test_df.user.isin(train_df.user)].to_csv(outtestfile2, index=False)
+	elif
+		field=='channel'
+		test_df[test_df.channel.isin(train_df.channel)].to_csv(outtestfile1, index=False)
+		test_df[~test_df.channel.isin(train_df.channel)].to_csv(outtestfile2, index=False)
